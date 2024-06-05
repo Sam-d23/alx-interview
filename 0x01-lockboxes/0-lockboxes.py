4a14d5b916fa1e8a29cfd6523210c5915f248947
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-05 12:34:11 +0300

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
-"""Solves the lock boxes puzzle."""
+'''Checks whether all the boxes can be opened
+'''
 
 
 def canUnlockAll(boxes):
-    """
-    Check if all boxes can be opened.
+    '''
+    Prototype: def canUnlockAll(boxes)\
+    boxes is a list of lists
+    A key labelled the same number as a box\
+            opens that box\
+    all keys are taken to be positive integers\
+        There can be keys that do not have boxes\
+    The first box boxes[0] is unlocked\
+    Return True if all boxes can be opened, else return False\
 
-    Args:
-        boxes (list): List which contain all the boxes with the keys.
-
-    Returns:
-        bool: True if all boxes can be opened, otherwise, False.
-    """
+    '''
     n = len(boxes)
-    seen = set()
-    stack = [0]
-
-    while stack:
-        current_box = stack.pop()
-        if current_box not in seen:
-            seen.add(current_box)
-            stack.extend(boxes[current_box])
-
-    return len(seen) == n
-
-
-def main():
-    """Entry point."""
-    canUnlockAll([[]])
-
-
-if __name__ == '__main__':
-    main()
-
+    checked_boxes = set([0])
+    unchecked_boxes = set(boxes[0]).difference(set([0]))
+    while len(unchecked_boxes) > 0:
+        boxIdx = unchecked_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unchecked_boxes = unchecked_boxes.union(boxes[boxIdx])
+            checked_boxes.add(boxIdx)
+    return n == len(checked_boxes)
