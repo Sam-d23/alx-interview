d61d9aed118cc5e9038c9eed099567ba50d9f32b
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-05 09:55:33 +0300

    task 01

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,21 +5,22 @@ def canUnlockAll(boxes):
     Determines if all boxes can be opened.
     
     Parameters:
-    boxes (list of lists): A list where each element is a list of keys contained in the corresponding box.
+    boxes (list of lists): A list where each element is a
+    list of keys contained in the corresponding box.
 
     Returns:
     bool: True if all boxes can be opened, False otherwise.
     """
-    n = len(boxes)  # Number of boxes
-    seen_boxes = set([0])  # Set to keep track of visited (unlocked) boxes
-    unseen_boxes = set(boxes[0]).difference(set([0]))  # Set of keys to be processed
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
 
     while unseen_boxes:
-        boxIdx = unseen_boxes.pop()
-        if boxIdx < 0 or boxIdx >= n:  # Ignore invalid box indices
+        box_idx = unseen_boxes.pop()
+        if box_idx < 0 or box_idx >= n:
             continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])  # Add new keys to be processed
-            seen_boxes.add(boxIdx)  # Mark this box as seen
+        if box_idx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[box_idx])
+            seen_boxes.add(box_idx)
 
-    return len(seen_boxes) == n  # Return True if all boxes have been seen
+    return len(seen_boxes) == n
