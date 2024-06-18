83e60c8bc23f7c2cb3a548eea5edb24ced31677a
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-18 20:08:58 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+
 import sys
 import signal
 import re
+
 
 total_file_size = 0
 status_code_counts = {"200": 0, "301": 0, "400": 0, "401": 0,
@@ -12,12 +14,14 @@ line_count = 0
 log_pattern = re.compile(r'^\S+ - \[\S+ \S+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$')
 
 def print_stats():
+    """Prints stats"""
     print(f"File size: {total_file_size}")
     for code in sorted(status_code_counts):
         if status_code_counts[code] > 0:
             print(f"{code}: {status_code_counts[code]}")
 
 def handle_interrupt(sig, frame):
+    """Handles interrupt"""
     print_stats()
     sys.exit(0)
 
