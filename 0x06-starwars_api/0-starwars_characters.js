2913928c10599a2ab2625c775eb861a9c282c56d
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-10 12:48:13 +0300

    SWAPI

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,18 +5,28 @@ const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) return console.log(err);
+    if (err) {
+      console.log(err);
+      return;
+    }
     
     const charactersURL = JSON.parse(body).characters;
-    const characterPromises = charactersURL.map(url => new Promise((resolve, reject) => {
+    const characterPromises = charactersURL.map((url) => new Promise((resolve, reject) => {
       request(url, (promiseErr, __, charactersReqBody) => {
-        if (promiseErr) return reject(promiseErr);
+        if (promiseErr) {
+          reject(promiseErr);
+          return;
+        }
         resolve(JSON.parse(charactersReqBody).name);
       });
     }));
 
     Promise.all(characterPromises)
-      .then(names => console.log(names.join('\n')))
-      .catch(console.log);
+      .then((names) => {
+        console.log(names.join('\n'));
+      })
+      .catch((allErr) => {
+        console.log(allErr);
+      });
   });
 }
