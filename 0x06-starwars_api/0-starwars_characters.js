66bd4bfb26b541cc477e1634cb2ba760328c988f
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-10 12:52:43 +0300

    SWAPI

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
@@ -7,26 +6,20 @@ if (process.argv.length > 2) {
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
       console.log(err);
-      return;
     }
-    
     const charactersURL = JSON.parse(body).characters;
-    const characterPromises = charactersURL.map((url) => new Promise((resolve, reject) => {
-      request(url, (promiseErr, __, charactersReqBody) => {
-        if (promiseErr) {
-          reject(promiseErr);
-          return;
-        }
-        resolve(JSON.parse(charactersReqBody).name);
-      });
-    }));
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-    Promise.all(characterPromises)
-      .then((names) => {
-        console.log(names.join('\n'));
-      })
-      .catch((allErr) => {
-        console.log(allErr);
-      });
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
