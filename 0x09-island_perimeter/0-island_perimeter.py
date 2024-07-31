f6846a36622787957fe42c564986f8a1cd9ab1dd
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-31 15:39:14 +0300

    Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,22 +5,27 @@ Function to calculate the perimeter of the island in the grid.
 
 
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of the island described in the grid.
-    """
+    """Calculates the perimeter of the island described in the grid."""
     perimeter = 0
+    if not isinstance(grid, list):
+        return 0
+
     rows = len(grid)
-    cols = len(grid[0])
-    
+
     for i in range(rows):
+        row = grid[i]
+        cols = len(row)
+
         for j in range(cols):
-            if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] == 0:
+            if row[j] == 1:
+                if i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0):
                     perimeter += 1
-                if i == rows - 1 or grid[i + 1][j] == 0:
+                if i == rows - 1 or (len(grid[i + 1]) > j and
+                                     grid[i + 1][j] == 0):
                     perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
+                if j == 0 or row[j - 1] == 0:
                     perimeter += 1
-                if j == cols - 1 or grid[i][j + 1] == 0:
+                if j == cols - 1 or (cols > j + 1 and row[j + 1] == 0):
                     perimeter += 1
+
     return perimeter
