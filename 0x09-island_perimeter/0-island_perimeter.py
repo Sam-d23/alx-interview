31f360fe21d8cf0c99824c7e260cc79441d61010
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-31 15:14:35 +0300

    Task

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,13 +13,12 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                # Check all four sides
-                if i == 0 or grid[i-1][j] == 0:  # Up
+                if i == 0 or grid[i-1][j] == 0:
                     perimeter += 1
-                if i == rows - 1 or grid[i+1][j] == 0:  # Down
+                if i == rows - 1 or grid[i+1][j] == 0:
                     perimeter += 1
-                if j == 0 or grid[i][j-1] == 0:  # Left
+                if j == 0 or grid[i][j-1] == 0:
                     perimeter += 1
-                if j == cols - 1 or grid[i][j+1] == 0: # Right
+                if j == cols - 1 or grid[i][j+1] == 0:
                     perimeter += 1
     return perimeter
