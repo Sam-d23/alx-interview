6c6987be2265e90741bfd5ca334b445623a36d3f
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-08-07 11:27:55 +0300

    Task

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,45 +4,51 @@ Prime Game Simulation
 """
 
 
-def isWinner(x, nums):
-    """Defining the main object."""
-    if x < 1 or not nums:
-        return None
-
-    max_n = max(nums)
+def sieve(max_n):
+    """Return a list indicating prime status for each number up to max_n"""
     is_prime = [True] * (max_n + 1)
     is_prime[0] = is_prime[1] = False
-
     for i in range(2, int(max_n ** 0.5) + 1):
         if is_prime[i]:
             for j in range(i * i, max_n + 1, i):
                 is_prime[j] = False
+    return is_prime
 
-    results = []
-    for n in nums:
-        if n == 1:
-            results.append('Ben')
-            continue
-
-        available = [True] * (n + 1)
-        turn = 0
-        while True:
-            move_made = False
-            for num in range(2, n + 1):
-                if is_prime[num] and available[num]:
-                    for multiple in range(num, n + 1, num):
-                        available[multiple] = False
-                    move_made = True
-                    break
-
-            if not move_made:
-                results.append('Ben' if turn == 0 else 'Maria')
+def simulate_game(n, is_prime):
+    """Simulate the game and return the winner for a given n"""
+    available = [True] * (n + 1)
+    turn = 0  # Maria starts first (0 for Maria, 1 for Ben)
+    
+    while True:
+        move_made = False
+        for num in range(2, n + 1):
+            if is_prime[num] and available[num]:
+                for multiple in range(num, n + 1, num):
+                    available[multiple] = False
+                move_made = True
                 break
 
-            turn = 1 - turn
+        if not move_made:
+            return 'Ben' if turn == 0 else 'Maria'
+        
+        turn = 1 - turn
 
-    maria_wins = results.count('Maria')
-    ben_wins = results.count('Ben')
+def isWinner(x, nums):
+    if x < 1 or not nums:
+        return None
+
+    max_n = max(nums)
+    is_prime = sieve(max_n)
+    
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        winner = simulate_game(n, is_prime)
+        if winner == 'Maria':
+            maria_wins += 1
+        else:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return 'Maria'
@@ -50,3 +56,4 @@ def isWinner(x, nums):
         return 'Ben'
     else:
         return None
+
